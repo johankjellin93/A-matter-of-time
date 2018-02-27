3e90095368cc0bac912a66e27a029e9ca6e5bbe9
Unknown <johankjellin93@gmail.com>
2018-02-28 00:01:06 +0100

    Use js to tell time

--- script.js
+++ script.js
@@ -2,5 +2,11 @@
 'use strict';
 
 
+document.addEventListener('DOMContentLoaded', function() {
+    var c = document.getElementById('current-time');
 
+    var d = new Date();
+    
+    c.innerHTML = d.toTimeString();
+});
 })();
