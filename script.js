e5bdefeb7d0837935d3c6c45566cd63de3c72a6c
Unknown <johankjellin93@gmail.com>
2018-02-28 00:28:05 +0100

    Polish the clock

--- script.js
+++ script.js
@@ -10,13 +10,27 @@ document.addEventListener('DOMContentLoaded', function() {
     function updateTime() {
         var d = new Date();
 
-        var hours = d.getHours();
-        if (hours > 12) hours -= - 12;
+        var hours = d.getHours(),
+        minutes = d.getMinutes(),
+        ampm = 'AM';
 
-        var sep = ':';
-        if (d.getSeconds() % 2 === 1) sep = ' ';
+        if (hours > 12) {
+            hours -= - 12;
+            ampm = 'PM';
+        } else if (hours === 0) {
+            hours = 12;
+        }
 
-        c.innerHTML = hours + sep + d.getMinutes();
+        if (minutes < 10) {
+            minutes = '0' + minutes;
+        }
+
+        var sepClass = '';
+        if (d.getSeconds() % 2 === 1) sepClass = 'trans';
+
+        var sep = '<span class="' + sepClass + '">:</span>';
+
+        c.innerHTML = hours + sep + minutes + ' ' + ampm;
     }
 });
 })();
