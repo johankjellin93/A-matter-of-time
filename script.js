97e3b4b858c8b2e658191ff6e1d6177b467d07dd
Unknown <johankjellin93@gmail.com>
2018-02-28 00:41:18 +0100

    challenge

--- script.js
+++ script.js
@@ -4,7 +4,8 @@
 
 document.addEventListener('DOMContentLoaded', function() {
     var c = document.getElementById('current-time');
-    
+    var day = document.getElementById('current-date');
+
     setInterval(updateTime, 1000);
 
     function updateTime() {
@@ -12,7 +13,48 @@ document.addEventListener('DOMContentLoaded', function() {
 
         var hours = d.getHours(),
         minutes = d.getMinutes(),
-        ampm = 'AM';
+        ampm = 'AM',
+        currentDay = d.getDay(),
+        dayString = '';
+
+        switch(currentDay) {
+            case 0:
+            dayString = 'January' + ' ';
+            break;
+            case 1:
+            dayString = 'February' + ' ';
+            break;
+            case 2:
+            dayString = 'Mars' + ' ';
+            break;
+            case 3:
+            dayString = 'April' + ' ';
+            break;
+            case 4:
+            dayString = 'Maj' + ' ';
+            break;
+            case 5:
+            dayString = 'June' + ' ';
+            break;
+            case 6:
+            dayString = 'July' + ' ';
+            break;
+            case 7:
+            dayString = 'August' + ' ';
+            break;
+            case 8:
+            dayString = 'September' + ' ';
+            break;
+            case 9:
+            dayString = 'October' + ' ';
+            break;
+            case 10:
+            dayString = 'November' + ' ';
+            break;
+            case 11:
+            dayString = 'December' + ' ';
+            break;
+        }
 
         if (hours > 12) {
             hours -= - 12;
@@ -30,7 +72,9 @@ document.addEventListener('DOMContentLoaded', function() {
 
         var sep = '<span class="' + sepClass + '">:</span>';
 
+        day.innerHTML = dayString + currentDay;
         c.innerHTML = hours + sep + minutes + ' ' + ampm;
+        
     }
 });
 })();
